75ab5abe3dfc2a6f556be7e37e5351d5ef4b8b9f
mooodo <fangang@aisino.com>
2019-02-01 17:29:53 +0800

    add the application

---
+++ src/main/java/com/demo/support/SupportApplication.java
@@ -0,0 +1,23 @@
+/* 
+ * Created by 2019年2月1日
+ */
+package com.demo.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The application for the support service.
+ * @author fangang
+ */
+@SpringBootApplication
+public class SupportApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SupportApplication.class, args);
+	}
+
+}
